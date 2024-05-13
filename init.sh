a5d247dc9de0fe24ef81e3056bcacf7eeba7fe1c
gravity-zero <gravity.neo@gmail.com>
2024-05-13 14:29:19 +0200

    correction import pasteries if not exist bakery db

--- init.sh
+++ init.sh
@@ -2,6 +2,19 @@
 
 echo "Lancement script"
 
-mongoimport --verbose -u root -p root --authenticationDatabase admin --collection pastries --type json --file /docker-entrypoint-initdb.d/pastries.json --jsonArray
+username="root"
+password="root"
+authenticationDatabase="admin"
+database="bakery"
+collection="pastries"
+
+sleep 5
+# Vérifie si la base de données bakery existe
+if ! mongosh --quiet -u root -p root --authenticationDatabase "admin" --eval "db.getMongo().getDBNames().indexOf('bakery') > -1" | grep true > /dev/null; then
+    echo "La base de données $database n'existe pas. Création en cours..."
+    mongoimport --verbose -u $username -p $password --authenticationDatabase $authenticationDatabase --db $database --collection $collection --type json --file /docker-entrypoint-initdb.d/pastries.json --jsonArray
+else
+    echo "La base de données $database existe déjà."
+fi
 
 echo "un jour viendra"
